248ed73175d4fdf380105286be92665ef8a077a3
Sarah Stanley <sarahmstanleyus@gmail.com>
2018-06-28 18:14:47 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray (array, element) {
+  return [element, ...array];
+}
+function destructivelyAddElementToBeginningOfArray (array, element){
+    array.unshift(element);
+    return array;
+}
+function addElementToEndOfArray (array, element) {
+  return element.push;
+} 
+function destructivelyAddElementToEndOfArray (array, element) {
+  array.shift(element);
+  return array;
+}
+function accessElementInArray (array, index)  {
+  return (chocolateBars [3]);
+}
